7c6d3d00b1dcada0af44999d6965e992481f7e74
Amy Wu <acagaga@gmail.com>
2016-07-25 11:42:18 -0400

    Stage 2

--- winerama/reviews/views.py
+++ winerama/reviews/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.contrib.auth.decorators import login_required
 from  .models import Review,Wine
 from .forms import ReviewForm
 import datetime
@@ -29,6 +30,7 @@ def wine_detail(request,wine_id):
     wine=get_object_or_404(Wine,pk=wine_id)
     return render(request,'reviews/wine_detail.html',{'wine':wine})
 
+@login_required
 def add_review(request,wine_id):
     #use the request url wine ID to look for the wine we are going to add the review to. 
     #it will redirect the view to a 404 page if it doesn't find it  
@@ -39,6 +41,7 @@ def add_review(request,wine_id):
         rating=form.cleaned_data['rating']
         comment=form.cleaned_data['comment']
         user_name=form.cleaned_data['user_name']
+        user_name=request.user.username
         review=Review()
         review.wine=wine
         review.user_name=user_name
@@ -52,3 +55,11 @@ def add_review(request,wine_id):
         return HttpResponseRedirect(reverse('reviews:wine_detail',args=(wine.id,)))
 
     return render(request,'reviews/wine_detail.html',{'wine':wine,'form':form})
+
+
+def user_review_list(request,username=None):
+    if not username:
+        username=request.user.username
+    latest_review_list=Review.objects.filter(user_name=username).order_by('-pub_date')
+    context={'latest_review_list':latest_review_list,'username':username}
+    return render(request,'reviews/user_review_list.html',context)
--- winerama/winerama/settings.py
+++ winerama/winerama/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'bootstrap3',
     'reviews',
+    'registration',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'winerama.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL='/reviews/review/user'
+ACCOUNT_ACTIVATION_DAYS=7
+REGISTRATION_AUTO_LOGIN=True
